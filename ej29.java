b862042ec5b3700d7440e10f12f2524ce44c2af1
eacz <estebanacanteros@gmail.com>
2021-04-08 19:12:46 -0300

    ej29

---
+++ ej29.java
@@ -0,0 +1,52 @@
+package ej;
+
+import java.util.Scanner;
+
+public class ej29 {
+    public static void main(String args[]) {
+        Scanner e = new Scanner(System.in);
+        int anioNac, diaNac=0, mesNac=0, anioAct, diaAct=0, mesAct=0, edad;
+        // fecha de nacimento
+        while(diaNac <=0 || diaNac > 31) {
+            System.out.println("Ingrese su dia de nacimiento"); 
+            diaNac = e.nextInt();
+        }
+        while(mesNac <= 0 || mesNac > 12) {
+            System.out.println("Ingrese su mes de nacimiento");
+            mesNac = e.nextInt();
+        }
+        System.out.println("Ingrese su año de nacimiento");
+        anioNac = e.nextInt();
+        
+        // fecha actual
+        while (diaAct <= 0 || diaAct > 31){
+            System.out.println("Ingrese el dia actual");
+            diaAct = e.nextInt();
+        }
+        while (mesAct <= 0 || mesAct > 12){
+            System.out.println("Ingrese el mes actual");
+            mesAct = e.nextInt();
+        }
+
+        System.out.println("Ingrese el año actual");
+        anioAct = e.nextInt();
+        
+        edad = anioAct - anioNac;
+        
+        if(mesNac > mesAct || (mesNac == mesAct && diaNac < diaAct)) {
+           edad--;
+        }
+        
+        if(anioAct == anioNac){
+            if(mesAct == mesNac){
+                edad = diaAct-diaNac;
+                System.out.println("La edad es: " + edad + " dias");
+            } else {
+                edad = mesAct-mesNac;
+                System.out.println("La edad es: " + edad + " meses");
+            }
+        } else {
+            System.out.println("La edad es: " + edad);
+        }
+    }
+}
